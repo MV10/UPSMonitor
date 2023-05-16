285dc089c6fc0976395a73a8a5c35cf93d1fabef
Jon McGuire <mcguirev10@gmail.com>
2023-05-16 10:40:30 -0400

    add WinRM dep to cmd, link to blog article

--- UPSMonitorService/Create.cmd
+++ UPSMonitorService/Create.cmd
@@ -1,6 +1,6 @@
 @openfiles.exe 1>nul 2>&1
 @if not %errorlevel% equ 0 goto :fail
-sc create "UPSMonitorService" binpath= "C:\Program Files\UPSMonitor\UPSMonitorService.exe" start= delayed-auto displayname= "McGuireV10.com UPS monitoring service"
+sc create "UPSMonitorService" binpath= "C:\Program Files\UPSMonitor\UPSMonitorService.exe" start= delayed-auto depend= WinRM displayname= "McGuireV10.com UPS monitoring service"
 @goto:eof
 :fail
 @echo:
